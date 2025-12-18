05568fe37b94a39c218e9283470821d8c9108e26
Andrey Vasilyev <ampcult@gmail.com>
2025-12-18 16:44:14 +0300

    GD-004
    * Аргументы и опции

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,11 +2,26 @@ package hexlet.code;
 
 import java.util.concurrent.Callable;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class Differ implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format",
+            showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    private String format = "stylish";
 
     @Override
     public Integer call() throws Exception {
