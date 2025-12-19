6b24b1742dd5644aedaaa05526a9f49aed73f574
Andrey Vasilyev <ampcult@gmail.com>
2025-12-19 11:43:13 +0300

    GD-006
    * Сравнение плоских файлов (JSON)

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,9 @@
 package hexlet.code;
 
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
@@ -16,6 +18,11 @@ import tools.jackson.databind.ObjectMapper;
 public class Differ implements Callable<Integer> {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final String INDENT = "    ";
+    private static final String INDENT_WITHOUT_SIGN = INDENT + "  ";
+    private static final String COLON_SYMBOL = ": ";
+    private static final String MINUS_SIGN = "- ";
+    private static final String PLUS_SIGN = "+ ";
 
     @Parameters(index = "0", description = "path to first file")
     private String filePath1;
@@ -33,13 +40,99 @@ public class Differ implements Callable<Integer> {
     public Integer call() throws Exception {
         Map<String, String> map1 = getData(filePath1);
         Map<String, String> map2 = getData(filePath2);
-        System.out.println(map1.toString());
-        System.out.println(map2.toString());
+        System.out.println(generate(map1, map2));
         return 0;
     }
 
-    private Map<String, String> getData(String filePath) {
-        return MAPPER.readValue(Path.of(filePath), new TypeReference<>() {
+    private static Map<String, String> getData(String filePath) {
+        return MAPPER.readValue(Path.of(filePath), new TypeReference<SortedMap<String, String>>() {
         });
+    }
+
+    private static String generate(Map<String, String> map1, Map<String, String> map2) {
+        StringBuffer result = new StringBuffer();
+
+        Iterator<Map.Entry<String, String>> it1 = map1.entrySet().iterator();
+        Iterator<Map.Entry<String, String>> it2 = map2.entrySet().iterator();
+
+        Map.Entry<String, String> e1 = it1.hasNext() ? it1.next() : null;
+        Map.Entry<String, String> e2 = it2.hasNext() ? it2.next() : null;
+
+        result.append("{").append(System.lineSeparator());
+
+        while (e1 != null || e2 != null) {
+
+            if (e1 != null && e2 != null) {
+                int cmp = e1.getKey().compareTo(e2.getKey());
+
+                if (cmp == 0) {
+                    // ключ есть в обоих
+                    String key = e1.getKey();
+                    String oldValue = e1.getValue();
+                    String newValue = e2.getValue();
+
+                    if (oldValue.equals(newValue)) {
+                        addKeyValueWithoutChange(result, key, newValue);
+                    } else {
+                        addChangedKeyValue(result, key, oldValue, newValue);
+                    }
+
+                    e1 = it1.hasNext() ? it1.next() : null;
+                    e2 = it2.hasNext() ? it2.next() : null;
+
+                } else if (cmp < 0) {
+                    // ключ удалён (есть только в map1)
+                    addDeletedKeyValue(result, e1.getKey(), e1.getValue());
+
+                    e1 = it1.hasNext() ? it1.next() : null;
+
+                } else {
+                    // ключ добавился (есть только в map2)
+                    addNewKeyValue(result,e2.getKey(), e2.getValue());
+
+                    e2 = it2.hasNext() ? it2.next() : null;
+                }
+
+            } else if (e1 != null) {
+                // остались только удалённые
+                addDeletedKeyValue(result, e1.getKey(), e1.getValue());
+
+                e1 = it1.hasNext() ? it1.next() : null;
+
+            } else {
+                // остались только добавленные
+                addNewKeyValue(result, e2.getKey(), e2.getValue());
+
+                e2 = it2.hasNext() ? it2.next() : null;
+            }
+        }
+
+        result.append("}");
+        return result.toString();
+    }
+
+    private static void addChangedKeyValue(StringBuffer buffer, String key, String oldValue, String newValue) {
+        addDeletedKeyValue(buffer, key, oldValue);
+        addNewKeyValue(buffer, key, newValue);
+    }
+
+    private static void addNewKeyValue(StringBuffer buffer, String key, String value) {
+        buffer.append(INDENT)
+                .append(PLUS_SIGN).append(key)
+                .append(COLON_SYMBOL).append(value)
+                .append(System.lineSeparator());
+    }
+
+    private static void addDeletedKeyValue(StringBuffer buffer, String key, String value) {
+        buffer.append(INDENT)
+                .append(MINUS_SIGN).append(key)
+                .append(COLON_SYMBOL).append(value)
+                .append(System.lineSeparator());
+    }
+
+    private static void addKeyValueWithoutChange(StringBuffer buffer, String key, String newValue) {
+        buffer.append(INDENT_WITHOUT_SIGN)
+                .append(key).append(COLON_SYMBOL).append(newValue)
+                .append(System.lineSeparator());
     }
 }
